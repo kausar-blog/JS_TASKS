d99cdd11996a321331209e3bca1f12e522564188
kausar-blog <kausarprogrammer@gmail.com>
2026-02-02 04:51:54 +0600

    js-problems-part1 2 tasks

--- js-problems-part1-practice-tasks/script.js
+++ js-problems-part1-practice-tasks/script.js
@@ -11,13 +11,16 @@ function temperature(cel) {
 
 function findArray(numbers) {
   // console.log(numbers);
-  let count = [];
+  let findNumber = 5;
+  let count = 0;
   for (const number of numbers) {
     // console.log(number);
-    if (count.includes(number) === false) {
-      console.log(number);
+    if (findNumber === number) {
+      // console.log(number);
+      count++;
     }
   }
+  return count;
 }
 
 const numbers = [5, 6, 11, 12, 98, 5];
@@ -25,16 +28,31 @@ const result = findArray(numbers);
 console.log(result);
 // result;
 
+// best
+function countOccurrences(numbers, findNumber) {
+  let count = 0;
+  for (const number of numbers) {
+    if (number === findNumber) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const num = [5, 6, 11, 12, 98, 5];
+console.log(countOccurrences(numbers, 5)); // 2
+console.log(countOccurrences(numbers, 25)); // 0
+
 // => 5
 
 console.log("");
 function randomEl() {
   const math = Math.random() * 20;
   if (Math.random() >= 10 || Math.random() <= 20) {
-    console.log(math);
+    // console.log(math);
   }
 }
 
-const kausar = randomEl();
+// const kausar = randomEl();
 
-console.log(kausar);
+// console.log(kausar);
